9f5d79336b4f55dc8db33783e2d7148ee1834951
JohnVibe <johnsmithgacacoc24@gmail.com>
2026-01-16 09:46:21 +0100

    Incluyendo proyecto y modelo de notas. Protección de cambios.

---
+++ app/src/main/java/dev/juangm14/notesapp/feature_note/domain/model/Note.kt
@@ -0,0 +1,18 @@
+package dev.juangm14.notesapp.feature_note.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import dev.juangm14.notesapp.ui.theme.*
+
+@Entity //For Room
+data class Note(
+val title: String,
+    val content: String,
+    val color: Int,
+    val timestamp: Long,
+    @PrimaryKey val id: Int? = null
+){
+    companion object {
+        val noteColors = listOf(RedOrange, LightGreen, Violet, BabyBlue, RedPink)
+    }
+}
